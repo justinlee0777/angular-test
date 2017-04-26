9bf4f79355f14dcbbde355aabc20daa56a1a7a7e
Justin Lee <justinlee0777@gmail.com>
2017-04-26 16:28:13 -0400

    Animation on 'select' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent }   from './dashboard/dashboard.component';
@@ -15,6 +16,7 @@ import { UIPagePathComponent } from './uiPagePath/uiPagePath.component';
 @NgModule({
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule
   ],
   declarations: [
--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,16 +1,30 @@
 import { Component, Input } from '@angular/core';
+import {
+	trigger,
+	state,
+	style,
+	animate,
+	transition
+} from '@angular/animations';
 
 @Component({
 	selector: 'sample-app-select',
 	template: `
 		<div class=select>
 			<div class=name [ngClass]="{ focused: focused }" (click)="onClick()">{{ label }}</div>
-			<div class=dropdown [hidden]=!focused>
+			<div class=dropdown [@show]="focused.toString()">
 				<div class=option *ngFor="let option of options" (click)="onClickOption()" [routerLink]="[option.route]">{{ option.label }}</div>
 			</div>
 		</div>
 	`,
-	styleUrls: [ './select.component.css' ]
+	styleUrls: [ './select.component.css' ],
+	animations: [
+		trigger('show', [
+			state('true' , style({ opacity: 1 })),
+			state('false', style({ opacity: 0, width: 0, height: 0  })),
+			transition('false <=> true', animate('100ms')),
+		])
+	]
 })
 export class SampleAppSelectComponent {
 	@Input() label: string;
